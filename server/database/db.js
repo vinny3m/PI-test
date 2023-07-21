f9a11286c427cecc590b6f76443bf0f3b23182a8
Vin <vineelamvmp123@gmail.com>
2023-07-21 17:04:01 +0530

    db connection

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose"
 
 const Connection= async () =>{
-    
+    //const URL=`mongodb://user:vineela@ac-j9s5vkl-shard-00-00.6ry7bsq.mongodb.net:27017,ac-j9s5vkl-shard-00-01.6ry7bsq.mongodb.net:27017,ac-j9s5vkl-shard-00-02.6ry7bsq.mongodb.net:27017/?ssl=true&replicaSet=atlas-bybrih-shard-0&authSource=admin&retryWrites=true&w=majority`;
+    // const URL=`mongodb://${username}:${password}@ac-j9s5vkl-shard-00-00.6ry7bsq.mongodb.net:27017,ac-j9s5vkl-shard-00-01.6ry7bsq.mongodb.net:27017,ac-j9s5vkl-shard-00-02.6ry7bsq.mongodb.net:27017/?ssl=true&replicaSet=atlas-bybrih-shard-0&authSource=admin&retryWrites=true&w=majority`;
     const URL= "mongodb://mongo:27017/test";
 
     try{
